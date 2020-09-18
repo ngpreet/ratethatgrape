07a0d506806ac38f3fc23bf8e875a299462760d3
Gurpreet Singh <ng.gurpreet@gmail.com>
2020-09-18 17:41:00 +0530

    url changed

--- server/config.js
+++ server/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    DBUrl: 'mongodb://localhost:27017/ratethatgrape'
+    //DBUrl: 'mongodb://localhost:27017/ratethatgrape'
+    DBUrl: 'mongodb://nav:preet123@ds331145.mlab.com:31145/ratethatgrape'
 }
